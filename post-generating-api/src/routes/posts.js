34d78c3a50655a794da68a221e62252a3d52df88
DimathJayasinghe <dimathjayasinghe@gmail.com>
2025-11-09 23:11:47 +0530

    Converted

--- post-generating-api/src/routes/posts.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { generate } = require('../controllers/postsController');
-
-const router = Router();
-
-// POST /api/generate
-router.post('/generate', generate);
-
-module.exports = router;
